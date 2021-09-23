ad1429915361cbbd43386c2d9b22e34bd13509d1
Jacob Starr <jakestarr60@gmail.com>
2021-09-23 11:30:49 -0400

    update running check to work on macos/linux

--- main/helpers/helpers.ts
+++ main/helpers/helpers.ts
@@ -1,16 +1,7 @@
-import {exec} from "child_process";
+import find from "find-process";
 
 export async function isRunning(query: String, callback: Function){
-    let platform = process.platform;
-    let cmd = '';
-    switch (platform) {
-        case 'win32' : cmd = `tasklist`; break;
-        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
-        case 'linux' : cmd = `ps -A`; break;
-        default: break;
-    }
-    exec(cmd, (err, stdout, stderr) => {
-        callback(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
-    });
+    let res = await find("name", query.toLowerCase());
+    callback(res.length > 0);
 }
 export default "";
